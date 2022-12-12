9fc25f7dea4a10bc17f23e1d42e16f156a1e2747
chetankotekar <chetankotekar.kotekar@gmail.com>
2022-12-12 19:04:06 +0530

    fixed and updated resume

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Test Engineer", "Software Developer", "Gamer", "Designer", "Writer"],
+        strings: ["Software Developer", "Gamer", "Designer", "Writer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Test Engineer", "Software Developer", "Gamer", "Designer", "Writer"],
+        strings: ["Software Developer", "Gamer", "Designer", "Writer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
